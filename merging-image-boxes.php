600a56ec7c969d1a6e3b20eadd2dc05ca676eecc
graphicedit <graphicedit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-14 14:37:27 +0000

    adding Merging Image Boxes by graphicedit
    git-svn-id: https://plugins.svn.wordpress.org/merging-image-boxes/trunk@451109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- merging-image-boxes.php
+++ merging-image-boxes.php
@@ -7,7 +7,7 @@ Version: 1.0.0
 Author: GraphicEdit
 Author URI: http://www.graphicedit.com/
 
-	Copyright 2011  GraphicEdit  (email : contact@graphicedit.com)
+	Copyright 2011  GraphicEdit (email : contact@graphicedit.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
